47a296fdd3e7f8d8fe576400ddcfdb09d768e302
Bmalik73 <bmalik01@live.fr>
2025-03-09 21:35:14 +0100

    feat: création du contrôleur d'API

---
+++ backend/src/Controller/UrlController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Controller;
+
+use App\Dto\UrlInput;
+use App\Exception\UrlNotFoundException;
+use App\Service\UrlService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+#[Route('/api', name: 'api_')]
+class UrlController extends AbstractController
+{
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
+    private UrlService $urlService;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+        UrlService $urlService
+    ) {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+        $this->urlService = $urlService;
+    }
+
+    /**
+     * Création d'une URL raccourcie
+     */
+    #[Route('/urls', name: 'create_url', methods: ['POST'])]
+    public function createUrl(Request $request): JsonResponse
+    {
+        // Désérialiser la requête en DTO
+        /** @var UrlInput $urlInput */
+        $urlInput = $this->serializer->deserialize(
+            $request->getContent(),
+            UrlInput::class,
+            'json'
+        );
+
+        // Valider les données d'entrée
+        $violations = $this->validator->validate($urlInput);
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[$violation->getPropertyPath()] = $violation->getMessage();
+            }
+            
+            return $this->json(['errors' => $errors], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Créer l'URL raccourcie
+        $urlOutput = $this->urlService->createShortUrl($urlInput);
+
+        return $this->json($urlOutput, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Récupération des informations d'une URL raccourcie
+     */
+    #[Route('/urls/{code}', name: 'get_url', methods: ['GET'])]
+    public function getUrl(string $code): JsonResponse
+    {
+        try {
+            $urlOutput = $this->urlService->getUrlInfo($code);
+            return $this->json($urlOutput);
+        } catch (UrlNotFoundException $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Redirection vers l'URL originale
+     */
+    #[Route('/{code}', name: 'redirect_url', methods: ['GET'])]
+    public function redirectToOriginalUrl(string $code): Response
+    {
+        try {
+            $originalUrl = $this->urlService->getOriginalUrl($code);
+            return $this->redirect($originalUrl);
+        } catch (UrlNotFoundException $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Recherche de l'URL originale à partir d'un code
+     */
+    #[Route('/lookup', name: 'lookup_url', methods: ['POST'])]
+    public function lookupUrl(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        
+        if (!isset($data['code']) || empty($data['code'])) {
+            return $this->json(['error' => 'Le code est requis'], Response::HTTP_BAD_REQUEST);
+        }
+        
+        $code = $data['code'];
+        
+        try {
+            $urlOutput = $this->urlService->getUrlInfo($code);
+            return $this->json($urlOutput);
+        } catch (UrlNotFoundException $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+}
